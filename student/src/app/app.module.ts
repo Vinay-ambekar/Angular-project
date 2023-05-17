b34db93d4c069824aacdb126c510c8f2aa6039cd
Vinay-ambekar <vinayambekar.b@gmail.com>
2023-05-17 14:01:22 +0530

    'commit'

--- student/src/app/app.module.ts
+++ student/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedComponent } from './shared/shared.component';
+import { PagesComponent } from './pages/pages.component';
+import { AddStudentComponent } from './pages/add-student/add-student.component';
+import { DisplayStudentComponent } from './pages/display-student/display-student.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SideNavComponent } from './shared/side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SharedComponent,
+    PagesComponent,
+    AddStudentComponent,
+    DisplayStudentComponent,
+    HomeComponent,
+    LoginComponent,
+    FooterComponent,
+    HeaderComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
