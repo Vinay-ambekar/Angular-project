a921e21e3fa6f157cc1c6226f2c9e5877d772dd0
Vinay-ambekar <vinayambekar.b@gmail.com>
2023-05-27 21:38:26 +0530

    'commit'

--- student/src/app/pages/login/login.component.ts
+++ student/src/app/pages/login/login.component.ts
@@ -6,19 +6,19 @@ import { Component ,EventEmitter, Input, Output} from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  @Input() appear: boolean = false;
-  @Output() buttonClick: EventEmitter<void> = new EventEmitter<void>();
+ @Input() appear: boolean = false;
+  @Output() buttonClick: EventEmitter<void> = new EventEmitter<void>(); 
 
   email:string=''
   password:string=''
 
   onButtonClick(): void {
-    if(this.email=="admin@gmail.com"&&this.password=="Admin"){
+     if(this.email=="admin@gmail.com"&&this.password=="Admin"){
       this.buttonClick.emit();
-      alert('you are loged in')
+      /* alert('you are loged in') */
     }else{
       alert('invalid credential')
-    }
-    
+    } 
+     
   }
 }
