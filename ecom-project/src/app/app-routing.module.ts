d8426d4180f812e31a848ed758240995427f4b0f
Vinay-ambekar <vinayambekar.b@gmail.com>
2023-05-29 18:28:55 +0530

    'commit'

--- ecom-project/src/app/app-routing.module.ts
+++ ecom-project/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { SellerHomeComponent } from './seller-home/seller-home.component';
 
 const routes: Routes = [
   {
@@ -12,7 +13,12 @@ const routes: Routes = [
   {
     path:'seller-auth',
     component:SellerAuthComponent
+  },
+  {
+    path:'seller-home',
+    component:SellerHomeComponent
   }
+ 
 ];
 
 @NgModule({
--- ecom-project/src/app/seller-auth/seller-auth.component.ts
+++ ecom-project/src/app/seller-auth/seller-auth.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-
+import { SellerService } from '../services/seller.service';
+import { Router } from '@angular/router';
+import { signup } from '../data-type';
 @Component({
   selector: 'app-seller-auth',
   templateUrl: './seller-auth.component.html',
   styleUrls: ['./seller-auth.component.scss']
 })
 export class SellerAuthComponent {
+  
+  constructor(private seller:SellerService , private router: Router){
 
-  signup(data:object):void{
-    console.log(data)
+  }
+
+  signup(data:signup):void{
+    
+    this.seller.userSignUp(data).subscribe((result)=>{
+      if(result){
+    this.router.navigate(['seller-home'])
+      }
+    });
+   
   }
 }
