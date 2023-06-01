ac91d944bf24974bef46b3dc4232784e4e506384
Vinay-ambekar <vinayambekar.b@gmail.com>
2023-06-01 09:43:21 +0530

    'commit'

--- ecom-project/src/app/home/home.component.ts
+++ ecom-project/src/app/home/home.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import { ProductService } from '../services/product.service';
+import { OnInit } from '@angular/core';
+import { product } from '../data-type';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+	popularProducts:undefined | product[]
+	//images = [1055, 194, 368].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
+	constructor(private product:ProductService) {
+		
+	}
+  ngOnInit(): void {
+     this.product.popularProducts().subscribe((data)=>{
+     console.warn(data)
+     this.popularProducts=data
+     })
+  }
 }
+
